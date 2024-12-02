b5bb63278cfc10fd19f84504268908e39d1999a7
TunahanTuze <tunahan.ttz@gmail.com>
2024-12-02 15:51:48 +0300

    algoritma bitti

---
+++ AsciiArt.java
@@ -0,0 +1,30 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+
+        System.out.println("--------------------------------------------------------- We welcome you with our visual called first project--------------------------------------------------------"+"\n"+"\n");
+
+        System.out.println(" +--^----------,--------,-----,-------^-,"+"\t\t\t\t\t\t                                 ,-----' |__,_~~___<'__`)-~__--__-~-> ");
+        System.out.println(" | |||||||||   `--------'     |          O"  + "   ---   ||   ---   \\\\\\\\\\\\   \\\\\\     "+"\t                                         | //  : | -__   ~__ o)____)),__ - '>  ) ");            
+        System.out.println(" `+---------------------------^--------|" +"\t\t\t\t\t\t                                 | //  : |- \\\\_ \\\\ -\\\\_\\\\ -\\\\ \\\\ \\ ~\\\\_  \\ ->");
+        System.out.println("   `\\_,---------,---------,----------'"+"\t\t\t\t\t\t\t                                 | //  : |_~_\\\\ -\\\\__\\\\ \\\\~'\\\\ \\\\ \\\\, \\\\__ .-");
+        System.out.println("     / XXXXXX /'|       /'"+"\t\t\t\t\t\t\t\t\t\t                 `-----._| `  -__`-- - ~~ -- ` --~> >");
+        System.out.println("    / XXXXXX /  `\\    /'"+"\t\t\t\t\t\t\t\t\t\t\t           _/___\\_    //)_`//  | ||]" );
+        System.out.println("   / XXXXXX /`-------'"+"\t\t\t\t\t\t\t\t\t\t\t\t     _____[_______]_[~~-_ (.L_/  ||");
+        System.out.println("  / XXXXXX /"+"\t\t\t\t\t\t\t\t\t\t\t\t            [____________________]' `\\_,/'/");
+        System.out.println(" / XXXXXX /"+"\t\t\t\t\t\t\t\t\t\t\t\t\t      ||| /          |||  ,___,'./");
+        System.out.println("(________("+"\t\t\t\t\t\t\t\t\t\t\t\t\t      ||| \\          |||,'______|");
+        System.out.println("ilktan hoca ekibi"+"\t\t\t\t\t\t\t\t\t\t\t\t      ||| /          /|| I==||");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t      ||| \\       __/_||  __||__");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t -----||-/------`-._/||-o--o---o---");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t   ~~~~~'");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t   BİZ ve ilk porjemiz");
+
+
+
+        
+        
+            
+        
+        
+    }
+}
